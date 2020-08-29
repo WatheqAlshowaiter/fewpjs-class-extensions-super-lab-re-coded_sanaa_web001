f2f21753e92f38b0c811139cda2a92d26ec39069
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-29 07:59:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,8 +31,25 @@ class Deciduous extends Tree {
   }
 }
 
+// 3.  Define a `Evergreen` class that extends `Tree`
 
-// oak = new Deciduous( 'Oak', 'Professor' );
-// console.log(Deciduous.definition());
-// "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.Deciduous trees shed their leaves annually."
-// "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually."
+//     - An `Evergreen` instance takes two parameters, `species` and `name`. Use
+//       `super()` in the constructor to use the parent class constructor to assign
+//       `species`, then assign `name` to a property in the `Evergreen`
+//       constructor
+
+//     - Create a static method, `definition()`, that uses `super` to access
+//       `definition()` from `Tree` and add the following to the provide a specific
+//       definition for `Evergreen`:
+
+//     > Evergreens keep their leaves all year round.
+
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name;
+  }
+  static definition() {
+    return super.definition() + " Evergreens keep their leaves all year round.";
+  }
+}
